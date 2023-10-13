440a2b43343e2a550a5684cad38de555f6308d62
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-13 15:55:41 -0500

    Declared items for gallery and gallery-items

--- src/app/landing/carousel/carousel.component.ts
+++ src/app/landing/carousel/carousel.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
+
+
+export class CarouselComponent implements OnInit{ 
+  
+//grabs the Ul element
+gallery: HTMLElement | null = document.querySelector(".js-gallery");
+if (gallery: { querySelectorAll: (arg0: string) => NodeListOf<HTMLElement>; }) {
+  // Grabs all the children elements
+  const galleryItems: NodeListOf<HTMLElement> = gallery.querySelectorAll(".gallery__item");
+  // How many images are in the slide to rotate through
+  const slideCount: number = 4;
+  // Width of all the images
+  const slideWidth: number = 1000;
+}
+
+ngOnInit(): void { 
 
 }
+}
+
+
+
+
