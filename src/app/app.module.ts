17b7942a0f6ea1c6f805074b483fced695ead4fc
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-13 14:38:21 -0500

    Refactor header componentto app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
