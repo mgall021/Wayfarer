b3b7e582fa9ac02852c32401691bbd7cf31a3157
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-16 12:00:02 -0500

    Completed Wayfarer-description

--- src/app/landing/wayfarer-description/wayfarer-description.component.ts
+++ src/app/landing/wayfarer-description/wayfarer-description.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-wayfarer-description',
   standalone: true,
+  imports: [NgFor],
   templateUrl: './wayfarer-description.component.html',
   styleUrls: ['./wayfarer-description.component.css'],
 
@@ -13,8 +14,8 @@ topics = [{
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor."
 },
   {
-    title: "a collaborative project",
-    body:  "mention our names"
+    title: "A collaborative project",
+    body:  "mention our names Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor."
   },
 {
   title:"Agile based project done in sprints",
