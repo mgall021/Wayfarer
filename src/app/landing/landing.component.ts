d5044e532daec6bbc319ae66126a158c86219fe7
avrubio <ariadna.vanegas@gmail.com>
2023-10-16 11:46:56 -0400

    Carousel Component added to landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CarouselComponent } from './carousel/carousel.component';
+
 @Component({
   selector: 'app-landing',
+  standalone: true,
+  imports: [CarouselComponent, NgbCarouselModule],
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
